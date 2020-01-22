0c5530d60a8d6a5b09e7818dd04951db821474c1
lucasmazo32 <lucasmazo32@gmail.com>
2020-01-22 17:35:41 -0500

    finished the advanced login

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,11 @@
 Rails.application.routes.draw do
   get 'sessions/new'
   get '/signup', to: 'users#new'
+  post '/signup',  to: 'users#create'
   get '/home', to: 'static_pages#home'
   get '/help', to: 'static_pages#help'
   get '/about', to: 'static_pages#about'
   get '/contact', to: 'static_pages#contact'
-  post '/signup',  to: 'users#create'
   root 'static_pages#home'
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
