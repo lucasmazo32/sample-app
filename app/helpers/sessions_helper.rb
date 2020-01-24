42b819672063105d88862aa53a4f5948d2d07297
lucasmazo32 <lucasmazo32@gmail.com>
2020-01-24 15:54:02 -0500

    finish the updated stuff

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -12,11 +12,16 @@ module SessionsHelper
       @current_user ||= User.find_by(id: user_id)
     elsif (user_id = cookies.signed[:user_id])
       user = User.find_by(id: user_id)
-      if user && user.authenticated?(cookies[:remember_token])
+      if user&.authenticated?(cookies[:remember_token])
         log_in user
         @current_user = user
       end
     end
+  end
+
+  # Returns true if the given user is the current user.
+  def current_user?(user)
+    user == current_user
   end
 
   def remember(user)
@@ -43,4 +48,15 @@ module SessionsHelper
     session.delete(:user_id)
     @current_user = nil
   end
+
+    # Redirects to stored location (or to the default).
+    def redirect_back_or(default)
+      redirect_to(session[:forwarding_url] || default)
+      session.delete(:forwarding_url)
+    end
+  
+    # Stores the URL trying to be accessed.
+    def store_location
+      session[:forwarding_url] = request.original_url if request.get?
+    end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_22_004455) do
+ActiveRecord::Schema.define(version: 2020_01_24_195256) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 2020_01_22_004455) do
     t.datetime "updated_at", null: false
     t.string "password_digest"
     t.string "remember_digest"
+    t.boolean "admin", default: false
   end
 
 end
